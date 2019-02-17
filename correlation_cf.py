18973ab84b402369629e9dbd3afe6d1a2ca2a6ca
Michael Sheroubi <45221187+msheroubi@users.noreply.github.com>
2019-02-17 02:51:03 -0800

    Commented code, new correlation formula

--- correlation_cf.py
+++ correlation_cf.py
@@ -1,3 +1,4 @@
+##Safe division, allows division by 0
 def div(x,y):
 	if(y==0):
 		return 0
@@ -22,7 +23,8 @@ def correlate(strA, strB):
 	adjectB = []
 	verbsB = []
 	prpB = []
-
+	
+	##Seperates nouns/questions, Verbs, Adjectives, and prepositions into seperate lists
 	for temp in strA:
 		if("NN" in temp[1]):
 			nounsA.append(temp[0])
@@ -36,7 +38,8 @@ def correlate(strA, strB):
 			adjectA.append(temp[0])
 		elif("PR" in temp[1]):
 			prpA.append(temp[0])
-
+	
+	#Checks through the second string and counts the number of matches for nouns and such
 	for temp in strB:
 		if("NN" in temp[1]):
 			nounsB.append(temp[0])
@@ -71,13 +74,19 @@ def correlate(strA, strB):
 	# 	+ div(v,len(verbsA))*(20*len(verbsA)/x) 
 	# 	+ div(j,len(adjectA))*20 
 	# 	+ div(p,len(prpA))*10)/(100)
-
+	
+	##"Tickle Down Weight Distribution": My formula for calculating the weights for different components of a sentence.
+	##It still needs more work, but as of right now it does its job
+	##Explanation: Weights are assigned by the ratio of components of A to B, and the difference of previous weights from the total constant
+	##		This constant doesn't matter too much, its the ratio between the weights that really matters.
+	##		In perfect conditions, nouns = 50, ajdects = 25, verbs = 12.5, perps = 6.25 and so on. "Trickle Down"
 	const = 100
 	w_n = (const/2)*div(N,len(nounsB))	
 	w_j = ((const-w_n)/2)*div(J,len(adjectB))
 	w_v = ((const-w_n - w_j)/2)*div(V,len(verbsB)) 
 	w_p = ((const-w_n - w_j -w_v)/2)*div(P,len(prpB))
 	w_x = w_n + w_j + w_v + w_p
-
+	
+	## u is calculated based on the ratio of matches to occurunces for each component multiplied by its respective weight
 	u = div((div(n,N)*w_n + div(v,V)*w_v + div(j,J)*w_j + div(p,P)*w_p), w_x)
 	return u
