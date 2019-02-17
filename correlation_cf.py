dcb70a06be1a063242713725676acd6c32d58d71
Michael Sheroubi <45221187+msheroubi@users.noreply.github.com>
2019-02-17 02:40:30 -0800

    Changed correlation, added question/answers
    Almost completely changed the formula for the correlation coefficient, added questions and answers, all that's left now is to add more questions/answers

--- correlation_cf.py
+++ correlation_cf.py
@@ -1,9 +1,3 @@
-
-nounsA = []
-adjectA = []
-
-verbsA = []
-prpA = []
 def div(x,y):
 	if(y==0):
 		return 0
@@ -19,11 +13,24 @@ def correlate(strA, strB):
 	j = 0
 	p = 0
 
+	nounsA = []
+	adjectA = []
+	verbsA = []
+	prpA = []
+
+	nounsB = []
+	adjectB = []
+	verbsB = []
+	prpB = []
 
 	for temp in strA:
 		if("NN" in temp[1]):
 			nounsA.append(temp[0])
+		elif("W" in temp[1]):
+			nounsA.append(temp[0])
 		elif("VB" in temp[1]):
+			verbsA.append(temp[0])
+		elif("IN" in temp[1]):
 			verbsA.append(temp[0])
 		elif("JJ" in temp[1]):
 			adjectA.append(temp[0])
@@ -32,17 +39,45 @@ def correlate(strA, strB):
 
 	for temp in strB:
 		if("NN" in temp[1]):
+			nounsB.append(temp[0])
 			if(temp[0] in nounsA):
 				n += 1
 		elif("VB" in temp[1]):
+			verbsB.append(temp[0])
 			if(temp[0] in verbsA):
 				v += 1
+		elif("IN" in temp[1]):
+			verbsB.append(temp[0])
+			if(temp[0] in verbsA):
+				v +=1
 		elif("JJ" in temp[1]):
+			adjectB.append(temp[0])
 			if(temp[0] in adjectA):
 				j += 1
 		elif("PR" in temp[1]):
+			prpB.append(temp[0])
 			if(temp[0] in prpA):
 				p += 1
-	x = len(strA)
-	u = (div(n,len(nounsA))*50 + div(v,len(verbsA))*30 + div(j,len(adjectA))*10 + div(p,len(prpA))*10)/100
-	return u 
+		elif("W" in temp[1]):
+			nounsB.append(temp[0])
+			if(temp[0] in nounsA):
+				n+=1
+
+	N = len(nounsA)
+	V = len(verbsA)
+	J = len(adjectA)
+	P = len(prpA)
+	# u = (div(n,len(nounsA))*(50*len(nounsA)/x) 
+	# 	+ div(v,len(verbsA))*(20*len(verbsA)/x) 
+	# 	+ div(j,len(adjectA))*20 
+	# 	+ div(p,len(prpA))*10)/(100)
+
+	const = 100
+	w_n = (const/2)*div(N,len(nounsB))	
+	w_j = ((const-w_n)/2)*div(J,len(adjectB))
+	w_v = ((const-w_n - w_j)/2)*div(V,len(verbsB)) 
+	w_p = ((const-w_n - w_j -w_v)/2)*div(P,len(prpB))
+	w_x = w_n + w_j + w_v + w_p
+
+	u = div((div(n,N)*w_n + div(v,V)*w_v + div(j,J)*w_j + div(p,P)*w_p), w_x)
+	return u
--- demo.py
+++ demo.py
@@ -1,27 +1,68 @@
 import tagNtokenize
 import correlation_cf as cf
 import time
+import pickle
 
-uIn = input("Enter text: ")
-uIn2 = input("Enter text 2: ")
-##print(uIn)
-##uIn = "Do you have a favorite team?"
-##uIn2 = "What is you favorite team?"
+with open('question_answers.pickle', 'rb') as f:
+        question_answers= pickle.load(f)
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+sub = ""
+while(True):
+        uIn = input("Enter text: ")
+##        uIn2 = input("Enter text 2: ")
+        ##print(uIn)
+        ##uIn = "Do you have a favorite team?"
+        ##uIn2 = "What is your favorite team?"
 
-tagtime = current_milli_time()
-tagged = tagNtokenize.tagNtokenize(uIn)
-tagtime = current_milli_time() - tagtime
-tagged2 = tagNtokenize.tagNtokenize(uIn2)
+        current_milli_time = lambda: int(round(time.time() * 1000))
 
-print(str(tagtime)+(" ms - tagtime"))
-##for t in tagged:
-##      print(t)
+        tagtime = current_milli_time()
+        tagged = tagNtokenize.tagNtokenize(uIn)
+        tagtime = current_milli_time() - tagtime
+##        tagged2 = tagNtokenize.tagNtokenize(uIn2)
 
-print(tagged)
-print(tagged2)
-corrtime = current_milli_time()
-print("u = " + str(cf.correlate(tagged,tagged2)))
-corrtime = current_milli_time() - corrtime
-print(str(corrtime) + (" ms - corrtime"))
+        max_u = 0.6
+        str_ans = "Sorry I don't quite know how to answer that!"
+
+        corrtime = current_milli_time()
+
+        # If in subquestions
+        if(sub != ""):
+                for quest in question_answers[sub]:
+##                      print("\n"+quest)
+                      tagged2 = tagNtokenize.tagNtokenize(quest)
+                      temp_u = cf.correlate(tagged,tagged2)
+##                      print("u= " + str(temp_u))
+                      if(temp_u>max_u):
+                              try:
+                                      str_ans = question_answers[sub][quest]
+##                                      print(tagged2)
+                                      max_u=temp_u
+                              except:
+                                      print("Error in Sub referencing")
+                                      sub = ""
+                                
+        for quest in question_answers:
+##                print("\n"+quest)
+                tagged2 = tagNtokenize.tagNtokenize(quest)
+                temp_u = cf.correlate(tagged,tagged2)
+##                print("u= " + str(temp_u))
+                if(temp_u>max_u):
+                        max_u=temp_u
+                        str_ans = question_answers[quest]['ans']
+##                        print(tagged2)
+                        sub = quest
+        corrtime = current_milli_time() - corrtime
+        
+##        print(str(tagtime)+(" ms - tagtime"))
+        ##for t in tagged:
+        ##      print(t)
+        
+##        print(tagged)
+##        print(tagged2)
+##        print("max_u = " + str(max_u))
+
+##        print("u = " + str(cf.correlate(tagged, tagged2)))
+                
+##        print(str(corrtime) + (" ms - corrtime"))
+        print("\nResponse>> " + str_ans)
